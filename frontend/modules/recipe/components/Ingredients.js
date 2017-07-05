ca5bf54f6530fd53ef2a4e08d6b280e9bdb18ce6
Ryan Noelk <ryannoelk@gmail.com>
2017-07-05 10:52:48 -0400

    Feature/serving updates (#202)
    * adding button style
    
    * removing react.createClass
    
    * adding some basic logic to update servings
    
    * updating directions and ingredients
    
    * fixing some weird state changes

--- frontend/modules/recipe/components/Ingredients.js
+++ frontend/modules/recipe/components/Ingredients.js
@@ -1,22 +1,35 @@
 import React from 'react'
 
-export default React.createClass({
-  getInitialState: function() {
-    return {
+class Ingredients extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
       data: this.props.data || []
     };
-  },
+
+    this.round = this.round.bind(this);
+  }
 
   componentWillReceiveProps(nextProps) {
     this.setState({data: nextProps.data});
-  },
+  }
 
-  render: function() {
-    var ingredients = this.state.data.map(function(ingredient) {
+  round(number, precision) {
+    let factor = Math.pow(10, precision);
+    let tempNumber = number * factor;
+    let roundedTempNumber = Math.round(tempNumber);
+    return roundedTempNumber / factor;
+  };
+
+  render() {
+    let ingredients = this.state.data.map(function(ingredient) {
+      let quantity = this.round(ingredient.quantity, 3);
+
       return (
         <li className="ingredient" key={ ingredient.id }>
           { (ingredient.quantity !== 0)
-              ? <span className="quantity">{ ingredient.quantity } </span>
+              ? <span className="quantity">{ quantity } </span>
               : null
           }
           { (ingredient.measurement)
@@ -29,11 +42,14 @@ export default React.createClass({
           }
         </li>
       );
-    });
+    }, this);
+
     return (
       <ul className="ingredients" >
         { ingredients }
       </ul>
     );
   }
-});
+}
+
+module.exports = Ingredients;
