b772cdf5be064a359bd9a5b5f5b964f8aaba9ed0
Ryan Noelk <ryannoelk@gmail.com>
2018-01-24 12:25:32 -0500

    Feature/image backup (#320)
    * Update sample_env_file_for_docker.list (#314)
    
    * Create backups.md
    
    * adding healthcheck to docker compose files and adding docs on how to take image backups
    
    * updating quickstart with image backup
    
    * updating gitignore

--- quick-start.py
+++ quick-start.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from time import sleep
+from os import getcwd
 from subprocess import call, Popen, PIPE
 
 
@@ -28,7 +29,7 @@ def start_containers():
     )
     output, err = p.communicate(b"input data that is passed to subprocess' stdin")
     if output and not err:
-        print("Take a database backup (saving as openeats.sql)...")
+        print("Taking a database backup (saving as openeats.sql)...")
         call(
             'docker exec openeats_db_1 sh -c ' +
             '\'exec mysqldump openeats -uroot -p"$MYSQL_ROOT_PASSWORD"\'' +
@@ -39,6 +40,20 @@ def start_containers():
         print("Creating the DB. This may take a minute...")
         call(['docker-compose', '-f', 'docker-prod.yml', 'up', '-d', 'db'])
         sleep(45)
+
+    p = Popen(
+        ['docker', 'ps', '-q', '-f', 'name=openeats_api_1'],
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=PIPE
+    )
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    if output and not err:
+        print("Taking a image backup save to 'site-media'...")
+        call(
+            'docker cp openeats_api_1:/code/site-media/ ' + getcwd(),
+            shell=True
+        )
 
     call(['docker-compose', '-f', 'docker-prod.yml', 'stop', 'nginx'])
     call(['docker-compose', '-f', 'docker-prod.yml', 'stop', 'api'])
